d98a5f7a3a73dde6a998e75628adb3183deddda0
msarvani14 <103956283+msarvani14@users.noreply.github.com>
2022-04-20 15:07:25 +0530

    Update fire truck code.py

--- fire truck code.py
+++ fire truck code.py
@@ -1,5 +1,3 @@
-# FIRE-TRUCK
-The Center City fire department collaborates with the transportation department to maintain maps of the city which reflects the current status of the city streets. On any given day, several streets are closed for repairs or construction. Firefighters need to be able to select routes from the firestations to fires that do not use closed streets. Central City is divided into non-overlapping fire districts, each containing a single firestation. When a fire is reported, a central dispatcher alerts the firestation of the district where the fire is located and gives a list of possible routes from the firestation to the fire. You must write a program that the central dispatcher can use to generate routes from the district fire stations to the fires.
 graph = [[] for item in range(22)]
 visited = [False] * 22
 possiblePaths = []
