8fbdcedfe949240d5622a3a6bd71558fca92d1a2
msarvani14 <103956283+msarvani14@users.noreply.github.com>
2022-04-20 15:03:27 +0530

    Create fire truck code.py

---
+++ fire truck code.py
@@ -0,0 +1,64 @@
+# FIRE-TRUCK
+The Center City fire department collaborates with the transportation department to maintain maps of the city which reflects the current status of the city streets. On any given day, several streets are closed for repairs or construction. Firefighters need to be able to select routes from the firestations to fires that do not use closed streets. Central City is divided into non-overlapping fire districts, each containing a single firestation. When a fire is reported, a central dispatcher alerts the firestation of the district where the fire is located and gives a list of possible routes from the firestation to the fire. You must write a program that the central dispatcher can use to generate routes from the district fire stations to the fires.
+graph = [[] for item in range(22)]
+visited = [False] * 22
+possiblePaths = []
+shortestpath = []
+
+
+# dfs multiple times
+def dfs(start, destination, path):
+    if (start == destination):
+        possiblePaths.append(path)
+        return
+    for item in graph[start]:
+        if (visited[item] == False):
+            visited[item] = True
+            dfs(item, destination, path + [item])
+            visited[item] = False
+
+print('Enter the Number of Destination StreetCorners :')
+testcases = int(input())
+for item in range(testcases):
+    print('Enter Destination StreetCorner :')
+    destination = int(input())
+    print("Enter Adjacent StreetCorners of Open Streets :")
+    while (True):
+        xCoOrd, yCoOrd = map(int, input().split())
+        if xCoOrd == 0 and yCoOrd == 0:
+            break
+        graph[xCoOrd].append(yCoOrd)
+        graph[yCoOrd].append(xCoOrd)
+    visited[1] = True
+    dfs(1, destination, [1])
+    visited[1] = False
+
+    print("CASE", str(item + 1) + ":")
+    for path in possiblePaths:
+        count = 0
+        for node in path:
+            print(node, end=" ")
+            count += 1
+        shortestpath.append(count)
+        print("\npathCount : ", count);
+    print("There are", len(possiblePaths), "routes from the firestation to streetcorner", str(destination) + ".")
+
+    minCount = float('inf')
+    for currentpathcount in shortestpath:
+        if (currentpathcount < minCount):
+            minCount = currentpathcount
+
+
+    print("\nMin Count, i.e Efficient Paths:")
+    for path in possiblePaths:
+        if (len(path) == minCount):
+            for item in path:
+                print(item, end=" ")
+            print()
+
+    print()
+
+    graph = [[] for item in range(22)]
+    visited = [False] * 22
+    possiblePaths = []
+    possiblePathCosts = []
